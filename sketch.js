6637d39356d202c3170c096e9a371734a6a35f0d
KEA KAMIYA <KSK23A@FSU.EDU>
2024-09-09 19:55:54 -0400

    added circle

--- sketch.js
+++ sketch.js
@@ -26,3 +26,27 @@ function draw() {
 
   ellipse(x1, y1, d1);
 }
+let x2 = 100;     // x position of the ball
+let y2 = 0;       // y position of the ball
+let x2Speed = 3;  // speed in X direction
+let y2Speed = 7; // speed in Y direction
+let d2 = 20;      // diameter of the ball
+
+function draw() {
+  background(220); // try commenting this out!
+ 
+  // if x hits the right side or the left side of the canvas,
+  // switch directions...
+  if (x2 > width || x2 < 0) {
+    x2Speed = x2Speed * -1;  
+  }
+  x2 = x2 + x2Speed; // adding a negative xSpeed *decreases* x, right?
+  
+  // same as above, but for the top and bottom of the canvas
+  if (y2 > width || y2 < 0) {
+    y2Speed = y2Speed * -1;  
+  }
+  y2 = y2 + y2Speed;
+
+  ellipse(x2, y2, d2);
+}

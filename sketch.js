0f71170251a00374c8ed91166b41731da7364d62
KEA KAMIYA <KSK23A@FSU.EDU>
2024-09-09 20:19:17 -0400

    two balls

--- sketch.js
+++ sketch.js
@@ -4,6 +4,12 @@ let x1Speed = 3;  // speed in X direction
 let y1Speed = 7; // speed in Y direction
 let d1 = 20;      // diameter of the ball
 
+let x2 = 150;     // x position of the ball
+let y2 = 50;       // y position of the ball
+let x2Speed = 3;  // speed in X direction
+let y2Speed = 7; // speed in Y direction
+let d2 = 20;      // diameter of the ball
+
 function setup() {
   createCanvas(400, 400);
 }
@@ -25,18 +31,7 @@ function draw() {
   y1 = y1 + y1Speed;
 
   ellipse(x1, y1, d1);
-}
-let x2 = 100;     // x position of the ball
-let y2 = 0;       // y position of the ball
-let x2Speed = 3;  // speed in X direction
-let y2Speed = 7; // speed in Y direction
-let d2 = 20;      // diameter of the ball
 
-function draw() {
-  background(220); // try commenting this out!
- 
-  // if x hits the right side or the left side of the canvas,
-  // switch directions...
   if (x2 > width || x2 < 0) {
     x2Speed = x2Speed * -1;  
   }
@@ -46,7 +41,8 @@ function draw() {
   if (y2 > width || y2 < 0) {
     y2Speed = y2Speed * -1;  
   }
-  y2 = y2 + y2Speed;
+  y2 = y2 + y1Speed;
 
   ellipse(x2, y2, d2);
 }
+

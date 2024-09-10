b575868425f448a519a62c4236e7a18b467cbd2d
KEA KAMIYA <KSK23A@FSU.EDU>
2024-09-10 14:06:10 -0400

    rect added

--- sketch.js
+++ sketch.js
@@ -8,7 +8,14 @@ let x2 = 150;     // x position of the ball
 let y2 = 50;       // y position of the ball
 let x2Speed = 3;  // speed in X direction
 let y2Speed = 7; // speed in Y direction
-let d2 = 20;      // diameter of the ball
+let d2 = 50;      // diameter of the ball
+
+let x3 = 110 
+let y3 = 5
+let x3Speed = 3
+let y3Speed = 7 
+
+
 
 function setup() {
   createCanvas(400, 400);
@@ -47,5 +54,20 @@ function draw() {
   }
   else {
     fill(0);   // Black
+  }
+  ellipse(x2, y2, d2); 
 
-  ellipse(x2, y2, d2); } }
+  
+  if (x3 > width || x3 < 0) {
+    x3Speed = x3Speed * -1;  
+  }
+  x3 = x3 + x3Speed; // adding a negative xSpeed *decreases* x, right?
+  
+  // same as above, but for the top and bottom of the canvas
+  if (y3 > width || y3 < 0) {
+    y3Speed = y3Speed * -1;  
+  }
+  y3 = y3 + y1Speed;
+
+  rect(x3, y3, 30, 50)
+}
